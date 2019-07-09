064b369792a16e3d5c9bcdaa1f02a143adb8efb0
Balaji M <51756211+BalajiM1603@users.noreply.github.com>
2019-07-09 10:06:10 +0530

    Create hellowordforntimes.c

---
+++ hellowordforntimes.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+  
+int n,i;
+  
+printf("enter how many times the word to be print:");
+  
+scanf("%d",&n);
+  
+for(i=0;i<=n;i++)
+  
+{
+      
+printf("Hello\n");
+      
+  
+}
+ 
+  
+  
+return 0;
+
+}
