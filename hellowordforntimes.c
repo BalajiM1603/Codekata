873afd12a98bdb1fcdd4b2e933902e5ad0041086
Balaji M <51756211+BalajiM1603@users.noreply.github.com>
2019-07-09 14:41:58 +0530

    Update hellowordforntimes.c

--- hellowordforntimes.c
+++ hellowordforntimes.c
@@ -1,25 +1,10 @@
 #include <stdio.h>
-
-
-int main()
-
+void main()
 {
-  
 int n,i;
-  
 scanf("%d",&n);
-  
 for(i=1;i<=n;i++)
-  
 {
-      
 printf("Hello\n");
-      
-  
 }
- 
-  
-  
-return 0;
-
 }
