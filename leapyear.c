663fc9276c25565b51b2e49c45334583db37b16c
Balaji M <51756211+BalajiM1603@users.noreply.github.com>
2019-07-09 13:30:39 +0530

    Update leapyear.c

--- leapyear.c
+++ leapyear.c
@@ -1,35 +1,17 @@
 #include <stdio.h>
- 
-
 int main()
-
 {
-  
 int year;
- 
- 
-  
 scanf("%d", &year);
- 
-  
 if (year%400 == 0)
-  
 { 
-    
 printf("yes");
-  
 }
-  else if (year%100 == 0) 
-  
+else if (year%100 == 0) 
 {
-    
 printf("no");
-  
 }
-  else if (year%4 == 0) 
-  
+else if (year%4 == 0) 
 {
-    
 printf("yes");
-  
 }
