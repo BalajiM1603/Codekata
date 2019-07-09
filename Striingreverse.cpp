ae7f2718c49ddc7f281a1a77aa26391760ad658b
Balaji M <51756211+BalajiM1603@users.noreply.github.com>
2019-07-09 21:00:28 +0530

    Create Striingreverse.cpp

---
+++ Striingreverse.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void print(string str)
+{
+    if(str.length()==0){
+    return;
+    }
+    print(str.substr(1));
+    cout<<str[0];
+}
+int main()
+{
+    cin>>str;
+    print("str");    
+    return 0;
+}
