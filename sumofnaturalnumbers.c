27ce69fc4fc4319b06f184bb01947be816722bc1
Balaji M <51756211+BalajiM1603@users.noreply.github.com>
2019-07-09 14:52:10 +0530

    Update sumofnaturalnumbers.c

--- sumofnaturalnumbers.c
+++ sumofnaturalnumbers.c
@@ -1,25 +1,10 @@
-#include <stdio.h>
-
-int main()
-
+main()
 {
-    
 int num, i, sum = 0;
-    
-    
-printf("Enter a number: ");
-    
 scanf("%d",&num);
-    
 for(i=1; i <= num; i++)
-    
 {
 sum += i;  
-    
 }
-    
-printf("Sum is: %d",sum);
-    
-return 0;
-
+printf("%d",sum);
 }
