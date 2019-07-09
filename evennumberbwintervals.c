8fc1fa4ee1a2ed7713ada1bcc8fc160c48fa5787
Balaji M <51756211+BalajiM1603@users.noreply.github.com>
2019-07-09 15:43:58 +0530

    Update evennumberbwintervals.c

--- evennumberbwintervals.c
+++ evennumberbwintervals.c
@@ -1,26 +1,12 @@
 #include <stdio.h>
-
 int main()
-
 {
-    
 int n1,n2,i;
-    
-printf("enter the number:");
-    
 scanf("%d %d",&n1,&n2);
-    
 for(i=n1+1;i<n2;i++)
-    
 {
-        
 if(i%2==0)
-      
-printf("%d\n",i);
-    
+printf("%d ",i);
 }
-    
-    
 return 0;
-
 }
