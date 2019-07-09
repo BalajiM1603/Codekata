600c3dbec6d8aefb4a476545d850172cd2123b31
Balaji M <51756211+BalajiM1603@users.noreply.github.com>
2019-07-09 13:22:46 +0530

    Update largestnumber.c

--- largestnumber.c
+++ largestnumber.c
@@ -9,14 +9,14 @@ int n1,n2,n3;
 
  scanf("%d %d %d",&n1,&n2,&n3);
     
-if(n1>n2 && n1>n3)
+if(n1>=n2 && n1>=n3)
     
 {
 printf("%d",n1);
   
 }
    
-else if(n2>n1 && n2>n3)
+else if(n2>=n1 && n2>=n3)
   
 {
       
@@ -24,7 +24,7 @@ printf("%d",n2);
 
 }
   
-else if(n3>n1 && n3>n2)
+else if(n3>=n1 && n3>=n2)
    
 {
        
