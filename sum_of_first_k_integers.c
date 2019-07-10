86dcea3a77034f701ba823af79e06d2ce06692c8
Balaji M <51756211+BalajiM1603@users.noreply.github.com>
2019-07-10 15:32:46 +0530

    Update sum_of_first_k_integers.c

--- sum_of_first_k_integers.c
+++ sum_of_first_k_integers.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int N,K,A[100],i,sum=0;
+    int N,K,arr[100],i,sum=0;
     scanf("%d\t%d",&N,&K);
     for(i=0;i<N;i++)
-        scanf("%d",&A[i]);
+        scanf("%d",&arr[i]);
     for(i=0;i<K;i++)
-        sum=sum+A[i];
+        sum=sum+arr[i];
     printf("%d",sum);
 }
