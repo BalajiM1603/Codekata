c435adf0aa7795be3f98976088067b50b338bc0e
Balaji M <51756211+BalajiM1603@users.noreply.github.com>
2019-07-10 19:45:21 +0530

    Create largest_number_in_array.c

---
+++ largest_number_in_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int a[100],n,i,largest;
+    scanf("%d",&n);
+    printf("enter the %d element in the array",n);
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+        largest=a[0];
+        for(i=1;i<n;i++)
+        {
+        if(largest<a[i])
+        {
+            largest=a[i];
+        }
+        }
+        printf("%d",largest);
+        return 0; 
+}
